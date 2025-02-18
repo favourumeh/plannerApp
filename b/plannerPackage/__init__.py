11dead8f1bc437db196e9973c1f514c5774d0a27
Favour Umeh <favourstpa@gmail.com>
2025-02-18 11:26:11 +0000

    Bug fix b/plannerPackage/__init__.py: Moved the routeHelper module imports to after the project_config module as the former is dependent on the latter.

--- b/plannerPackage/__init__.py
+++ b/plannerPackage/__init__.py
@@ -1,7 +1,10 @@
-from .bespoke_funcs import generate_config_dict
-from .routeHelper import token_required
-from .routeHelper import login_required
-from .routeHelper import update_refresh_token_table
+from plannerPackage.bespoke_funcs import generate_config_dict
 from .project_config import access_token_dur
 from .project_config import refresh_token_dur
 from .project_config import allowed_origins
+from .routeHelper import token_required
+from .routeHelper import login_required
+from .routeHelper import update_refresh_token_table
+
+#note: the dots are important. They tell python to look for the above modules in the directory of this file NOT the directory of the app execution command (i.e., dir = plannerPackage/b/  command = python main.py)
+    # If you don't want to use relative imports you can use absolute import like "plannerPackage.bespoke_func import generate_config_dict" instead (note: it assumes that the app execution dir is plannerApp/b/ and not plannerApp/)
