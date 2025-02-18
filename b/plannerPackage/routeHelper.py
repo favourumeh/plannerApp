566c2a9d024de91974674ba3653ca0cb26c90333
Favour Umeh <favourstpa@gmail.com>
2025-02-18 11:41:19 +0000

    Updated b/plannerPackage/routeHelper.py: 1) for token_required decorator: switched to extracting the access token from cookies as opposed to the "Authorization" header. 2) For login_required decorator: now prompts when used is not logged in.  3) for update_refresh_token_table func: fixed a bug in the db commit .

--- b/plannerPackage/routeHelper.py
+++ b/plannerPackage/routeHelper.py
@@ -1,12 +1,12 @@
 #This file defines helper functions/decorators for the routes
-from flask import request, jsonify, Flask
+from flask import request, jsonify, Flask, session
 from flask_sqlalchemy import SQLAlchemy
 import jwt
 from itsdangerous import URLSafeTimedSerializer
 from models import Refresh_Token
 from functools import wraps
 from datetime import datetime, timedelta, timezone
-from plannerPackage import refresh_token_dur
+from . import refresh_token_dur #alternative: from plannerPackage import refre...
 from uuid import uuid4
 from werkzeug.security import generate_password_hash
 from cryptography.fernet import Fernet
@@ -18,36 +18,40 @@ from dotenv import load_dotenv
 load_dotenv()
 
 #create authorisation decorator 
-def token_required(app: Flask):
+def token_required(app: Flask, serializer: URLSafeTimedSerializer):
     """
-    Checks the user's CRUD requests to determine if: 1) There is an 'Authorization' header; 2) If an access token is present; 3) if the token is valid.
+    Checks the user's CRUD requests to determine if: 1) There is a cookie containing the access token called 'session_AT';
+    2) The token is valid. 
+    Args:
+        app: Flask app instance
+        serializer: used to deserialize and verify the signature of the access token cookie 'session_AT'.
     """
     def decorated(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            resp_dict = {"message": "", "err":""}
-            
-            #Check if request contains "Authorization" header
-            auth_header = request.headers.get("Authorization")
-            if not auth_header:
-                resp_dict["message"] = "Request is missing 'Authorization' header."
+            resp_dict = {"message": ""}
+            #get access token from http-only cookie
+            access_token_cookie: str|None = request.cookies.get("session_AT")
+
+            if not access_token_cookie:
+                resp_dict["message"] = "Request is missing access token. Please login to refresh access token"
                 return jsonify(resp_dict), 400
-            
-            #Extract access token (Bearer) from "Authorization" header
-            token = auth_header.split(" ")[-1] if auth_header.startswith("Bearer ") else ""
-            if not token:
-                resp_dict["message"] = "Access Token missing! Please authenticate (login or refresh) to generate access token"
-                return jsonify(resp_dict), 401
-            
+            try:
+                access_token = serializer.loads(access_token_cookie)
+            except Exception as e:
+                resp_dict["message"] = f"Deserialisation or Signiture verificaiton of access token cookie has failed. Err Message: {e}"
+                return jsonify(resp_dict), 400
+
             #Validate access token
             try:
                 payload = jwt.decode(
-                    jwt = token,
+                    jwt = access_token,
                     key = app.config["SECRET_KEY"],
                     algorithms = "HS256"
                 )
             except Exception as e:
-                resp_dict["message"], resp_dict["err"] = "Invalid Access Token!", f"{e}"
+                resp_dict["message"] = f"Invalid Access Token! Reason: {e}"
+                return jsonify(resp_dict), 401
             return func(*args, **kwargs)
         return wrapper
     return decorated
@@ -58,15 +62,23 @@ def login_required(serializer: URLSafeTimedSerializer):
     def decorated(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            resp_dict = {"message": ""}           
-            encrypted_session_data: bytes = serializer.loads(request.cookies.get("bespoke_session")) 
+            resp_dict = {"message": ""}
+            cookie = request.cookies.get("bespoke_session")
+            
+            if not cookie:
+                resp_dict["message"] = "Currently logged out. Please login"
+                return jsonify(resp_dict), 400
+            encrypted_session_data: str = serializer.loads(cookie) 
             cipher = Fernet(os.environ["session_key"].encode())
-            decryoted_session_data: dict = json.loads(cipher.decrypt(encrypted_session_data).decode())
-            logged_in = decryoted_session_data["logged_in"]
-
+            decrypted_session_data: dict = json.loads(cipher.decrypt(encrypted_session_data.encode()).decode())
+            logged_in = decrypted_session_data["logged_in"]
+            
             if logged_in != "True":
                 resp_dict["message"] = "Please login!"
                 return jsonify(resp_dict), 401
+            
+            #Extract user id from the session data for use in downstream function
+            session["userID"] = decrypted_session_data["userID"]
             return func(*args, **kwargs)
         return wrapper
     return decorated
@@ -88,7 +100,7 @@ def update_refresh_token_table(action: str, refresh_token_obj: Refresh_Token|Non
     if action == "create":
         refresh_token_obj = Refresh_Token(token=token_hash, exp=exp, user_id=user_id)  
         db.session.add(refresh_token_obj)
-        db.session.commit(refresh_token_obj)
+        db.session.commit()
         return refresh_token_obj
 
     if action == "update":
