e46c70449f0d827dc2785227fed918121399b447
Favour Umeh <favourstpa@gmail.com>
2025-02-23 14:34:28 +0000

    Updated b/test/__init__.py: Added routes to the app that test the login_required. This should reduced the amount of test cases used in auth routes and other routes

--- b/test/__init__.py
+++ b/test/__init__.py
@@ -2,6 +2,9 @@ from routes import auth, project
 from config import app, serializer
 from models import db, User, Project, Refresh_Token
 from datetime import datetime
+from plannerPackage import login_required, token_required
+from flask import jsonify, Response
+from typing import Tuple
 
 #configure app
 app.config["TESTING"] = True # set to true so Exceptions can propagate to the test client (i.e. so we get HTTP status codes other than 500 when something goes wrong with client request)
@@ -9,5 +12,17 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///:memory:" #use an in-memory 
 app.register_blueprint(auth)
 app.register_blueprint(project)
 
-#bind db instance to flask app instance
+    #add these test routes which will only be used to test the login_required and token_requied decorators
+@app.route("/test-login-required", methods=["GET"])
+@login_required(serializer=serializer)
+def test_login() -> Tuple[Response, int]:
+    return jsonify({"message": "Success: login test completed"}), 200
+
+@app.route("/test-token-required", methods=["GET"])
+@token_required(app=app, serializer=serializer)
+def test_token() -> Tuple[Response, int]:
+    return jsonify({"message": "Success: token test completed"}), 200
+
+
+    #bind db instance to flask app instance
 db.init_app(app=app)
