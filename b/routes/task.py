e78a343d7712bd37fabfbadd98496e725947794b
Favour Umeh <favourstpa@gmail.com>
2025-02-26 12:19:05 +0000

    Bug fix b/routes/task.py: corrected 'methods' parameter input of the blueprint method "route".

--- b/routes/task.py
+++ b/routes/task.py
@@ -16,28 +16,28 @@ load_dotenv()
 
 #tasks routes 
 #create
-@task.route("/create-task", methods="POST")
+@task.route("/create-task", methods=["POST"])
 @login_required(serializer=serializer)
 @token_required(app=app, serializer=serializer)
 def create_task() -> Tuple[Response, int]:
     pass
     
 #read
-@task.route("/get-tasks", methods="GET")
+@task.route("/get-tasks", methods=["GET"])
 @login_required(serializer=serializer)
 @token_required(app=app, serializer=serializer)
 def read_tasks(): 
     pass
 
 #update
-@task.route("/edit-task/<int:task_id>", methods="PATCH")
+@task.route("/edit-task/<int:task_id>", methods=["PATCH"])
 @login_required(serializer=serializer)
 @token_required(app=app, serializer=serializer)
 def update_task(task_id: int) -> Tuple[Response, int]: 
     pass
 
 #delete
-@task.route("/delete-task/<int:task_id>", methods="DELETE")
+@task.route("/delete-task/<int:task_id>", methods=["DELETE"])
 @login_required(serializer=serializer)
 @token_required(app=app, serializer=serializer)
 def delete_task(task_id: int) -> Tuple[Response, int]:
