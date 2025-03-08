ac45d33ae7ad286b1b49a52882be7058cebe466e
Favour Umeh <favourstpa@gmail.com>
2025-03-08 13:38:46 +0000

    Updated f/s/App.css and f/s/App.jsx: Removed Vite default component content and styling and replaced it with plannerApp content.

--- f/src/App.jsx
+++ f/src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { backendBaseUrl } from './project_config'
+import { fetchProjects, fetchObjectives, fetchTasks } from './fetch_entities'
+import GuestPage from './c/guestPage'
+import globalContext from './context'
+// import Modal from "./c/modal.jsx"
+
+const persistState = (sessionName, default_) => {
+    var state = JSON.parse(sessionStorage.getItem(sessionName))
+    return state!=null? state:default_ 
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [isModalOpen, setIsModalOpen] = useState(() => persistState("isModalOpen",true))
+    const [isLoggedIn, setIsLoggedIn] = useState(() => persistState("isLoggedIn",false))
+    const [clientAction, setClientAction] = useState(() => persistState("clientAction","")) //sign-up, login, edit-notes, delete-notes
+    // const [currentUser, setCurrentUser] = useState(() => persistState("currentUser",{}))
+    // const [notificationMessage, setNotificationMessage] = useState("")
+    // const [isNotiBarVisible, setIsNotiBarVisible] = useState(false)
+    // const [isNotiMessageError, setIsNotiMessageError] = useState(false) // determines the colour of the noti message bar (green or red)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    //Update session storage object when state variable changes
+    useEffect(() => sessionStorage.setItem("isLoggedIn", JSON.stringify(isLoggedIn)), [isLoggedIn])
+    useEffect(() => sessionStorage.setItem("isModalOpen", JSON.stringify(isModalOpen)), [isModalOpen])
+    useEffect(() => sessionStorage.setItem("clientAction", JSON.stringify(clientAction)), [clientAction])
+    // useEffect(() => sessionStorage.setItem("currentUser", JSON.stringify(currentUser)), [currentUser])
+
+    // const handleLogin = (user) => {
+    //     setCurrentUser(user)
+    //     setIsLoggedIn(true)
+    //     setIsModalOpen(false)
+    //     setClientAction("")
+    // }
+        
+    // const handleLogout = () => {
+    //     setCurrentUser({})
+    //     setIsLoggedIn(false)
+    //     setClientAction("")
+    //     isModalOpen && setIsModalOpen(false) // && returns first falsey value. if both falsey then return last value
+    // }
+
+    // const handleNotification = (message, category) => {
+    //     setIsNotiBarVisible(true)
+    //     setNotificationMessage(message)
+    //     setIsNotiMessageError(category=="success"? false:true)
+    // } 
+
+    const globalProps = {
+        isModalOpen, setIsModalOpen,
+        clientAction, setClientAction,
+        // currentUser, setCurrentUser,
+        // notificationMessage, setNotificationMessage,
+        // isNotiBarVisible, setIsNotiBarVisible,
+        // isNotiMessageError, setIsNotiMessageError,
+        // handleNotification, fetchTasks, fetchObjectives, fetchProjects
+    }
+
+    return (
+        <>
+        <globalContext.Provider value={globalProps}>
+            <GuestPage isLoggedIn={isLoggedIn}/>
+            {/* <Modal>
+                <form>
+                    <label for="firstName"> First Name: </label>
+                    <input type="text"></input>
+                    <br/>
+                    <label for="firstName"> First Name: </label>
+                    <input type="text"></input>
+                    <br/>
+                    <label for="firstName"> First Name: </label>
+                    <input type="text"></input>
+                </form>
+            </Modal> */}
+        </globalContext.Provider>
+        </>
+    )
+    }
 
 export default App
