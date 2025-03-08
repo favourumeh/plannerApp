86ece8d33efbabf33e54ba518204e59d6c0da545
Favour Umeh <favourstpa@gmail.com>
2025-03-08 10:21:45 +0000

    Created f/src/project_config.js: Added logic that toggles between the backend api url in a prod and dev environment

---
+++ f/src/project_config.js
@@ -0,0 +1,15 @@
+console.log(import.meta.env) //env variables defined in f/.env #
+const backend_env = import.meta.env.VITE_BACKEND_ENV
+var backendBaseUrl //build fails if this is not decalared
+if (backend_env == "dev") {
+    backendBaseUrl = import.meta.env.VITE_DEV_BACKEND_API_URL
+}
+else if (backend_env == "prod") {
+    backendBaseUrl = import.meta.env.VITE_PROD_DOCKER_BACKEND_API_URL
+}
+else {
+    console.log("Error backendBaseURL is not specified. Check frontend/.env and frontend/src/project_config.js")
+}
+console.log("backend_env:", backend_env)
+console.log("backendbaseurl: ", backendBaseUrl )
+export {backendBaseUrl}
