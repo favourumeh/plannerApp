52a4bfb084bff15a9271c2e0dd8185bb61745093
Favour Umeh <favourstpa@gmail.com>
2025-03-17 11:11:27 +0000

    Updaetd f/src/fetch_entities.js: completed the fetch fucntions for projects and objectives

--- f/src/fetch_entities.js
+++ f/src/fetch_entities.js
@@ -1,21 +1,55 @@
 import { backendBaseUrl } from "./project_config"
 
-const fetchProjects = async () => {
-    return null
+const fetchUserProjects = async (setProjects, handleNotification, setDefaultProject) => {
+    const url = `${backendBaseUrl}/read-projects`
+    const options = {
+        method:"GET",
+        headers: {"Content-Type":"application/json"},
+        credentials:"include"
+    }
+    const resp = await fetch(url, options)
+    const resp_json = await resp.json()
+
+    if (resp.status == 200){
+    console.log(resp_json.message)
+    const projects = resp_json.projects
+    setProjects(projects)
+    setDefaultProject(projects.filter((project)=> project["type"]=="default project")[0])
+    // console.log(resp_json.projects)
+    } else {
+    console.log(resp_json.message)
+    handleNotification(resp_json.message, "failure")
+    }
 }
 
-const fetchObjectives = async () => {
-    return null
+const fetchUserObjectives = async (setObjectives, handleNotification, setDefaultProjectObjective) => {
+    const url = `${backendBaseUrl}/read-objectives`
+    const options = {
+        method:"GET",
+        headers: {"Content-Type":"application/json"},
+        credentials:"include"
+    }
+    const resp = await fetch(url, options)
+    const resp_json = await resp.json()
+
+    if (resp.status == 200){
+    console.log(resp_json.message)
+    const objectives = resp_json.objectives
+    setObjectives(objectives)
+    setDefaultProjectObjective(objectives.filter((objective) => objective["type"]=="default project objective")[0].id)
+    // console.log(resp_json.objectives)
+    } else {
+    console.log(resp)
+    handleNotification(resp_json.message, "failure")
+    }
 }
 
-const fetchTasks = async () => {
+const fetchUserTasks = async (setTasks, handleNotification) => {
     const url = `${backendBaseUrl}/read-tasks`
     const options = {
-        "method":"GET",
-        "headers": {
-          "Content-Type":"application/json"
-        },
-        "credentials":"include"
+        method:"GET",
+        headers: {"Content-Type":"application/json"},
+        credentials:"include"
     }
     const resp = await fetch(url, options)
     const resp_json = await resp.json()
@@ -23,9 +57,11 @@ const fetchTasks = async () => {
     if (resp.status == 200){
       console.log(resp_json.message)
       setTasks(resp_json.tasks)
+    //   console.log(resp_json.tasks)
     } else {
       console.log(resp)
+      handleNotification(resp_json.message, "failure")
     }
 }
 
-export {fetchProjects,fetchObjectives, fetchTasks}
+export {fetchUserProjects,fetchUserObjectives, fetchUserTasks}
