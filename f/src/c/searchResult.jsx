c3d3d3442cf3041ae24e3cf32299da82a113f703
Favour Umeh <favourstpa@gmail.com>
2025-03-23 15:49:02 +0000

    Created f/s/c/searchResult.jsx and .css: Created component that displays the result of project and object queries in the task and objective forms.

---
+++ f/src/c/searchResult.jsx
@@ -0,0 +1,43 @@
+import "./searchResult.css"
+import { useState, useEffect, useContext} from "react";
+import globalContext from "../context";
+
+function SearchResult({searchFieldLabel, query, setQuery, entityArray}) {
+    /*Queries a field ["title"] of an array of objects [entityArray] and returns the object(s) from the array that match the query*/
+    const {showProjectQueryResult, showObjectiveQueryResult} = useContext(globalContext)
+
+    const [queryResults, setQueryResult] =  useState()
+    useEffect(()=> {
+        var result = entityArray.filter((item) => 
+                        item["title"].toLowerCase().includes(query.toLowerCase()) || 
+                        item["title"]== (searchFieldLabel == "Project"?"No Project":"No Objective")).slice(0,5)
+
+        if (query==="" || typeof query==="undefined") {
+            result = entityArray.slice(0,5)
+        }
+        setQueryResult(result)}, [query, showProjectQueryResult, showObjectiveQueryResult])
+
+    if (!showProjectQueryResult && searchFieldLabel=="Project") {
+        return null
+    }
+    if (!showObjectiveQueryResult && searchFieldLabel=="Objective") {
+        return null
+    }
+
+    const onResultClick = (result) => {
+        setQuery(result.title)    
+    }
+
+    return (
+        <div className="query-result-overlay">
+            <ol className="query-result-list">
+                {queryResults.map((result) => 
+                    <li className="query-result" key={`${searchFieldLabel} - ${result.id}`} onClick={() => onResultClick(result)}>
+                        {result[`${searchFieldLabel.toLowerCase()}Number`]} {result["title"]}
+                    </li>
+                )}
+            </ol>
+        </div>
+    )
+}
+export default SearchResult
