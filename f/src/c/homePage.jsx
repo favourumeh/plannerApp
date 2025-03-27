fa139767855c520259b19211f7afca3d5b7b0b88
Favour Umeh <favourstpa@gmail.com>
2025-03-27 18:17:04 +0000

    Updated f/src/c/homePage.jsx: Refactored HomePage component to manage current date state. Added task filtering by date by adding functionality to day navigation buttons on home page .

--- f/src/c/homePage.jsx
+++ f/src/c/homePage.jsx
@@ -1,41 +1,58 @@
 import "./homePage.css"
 import { useState, useContext, useEffect } from "react"
 import globalContext from "../context"
-import { backendBaseUrl } from "../project_config"
 import TaskCard from "./taskCard"
 import Dropdown from "./Dropdown"
 import Header from "./header"
 
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let current_date = new Date()
-let currentDay = daysOfWeek[current_date.getDay()]
-
-function HomePage ({isLoggedIn}) {
-    const {
-        sitePage, setSitePage, setForm, handleDeleteEntity,
-        setIsModalOpen, tasks, handleRefresh} = useContext(globalContext)
+const todaysDate = new Date().toDateString()
+function HomePage ({isLoggedIn, sitePage, homePageTasks, setHomePageTasks}) {
 
     if (!isLoggedIn || sitePage!=="view-homepage") {
         return null
     }
+
+    const {currentDate, setCurrentDate, setSitePage, setForm, handleDeleteEntity, setIsModalOpen, tasks, objectives, projects, handleRefresh} = useContext(globalContext)
+    const [currentDay, setCurrentDay] = useState(null)
+
+    useEffect(() => {setCurrentDay(daysOfWeek[new Date(currentDate).getDay()])}, [currentDate])
+
+   useEffect(() => {
+        setHomePageTasks(tasks.filter(task => new Date(task.scheduledStart).toDateString() ===  new Date(currentDate).toDateString()))
+    }, [currentDate, tasks, objectives, projects])
 
     const handleCreateContent = (content) => {
         setForm(`create-${content}`)
         setIsModalOpen(true)
     }
 
+    const handleDayNavigation = (direction) => {
+        switch (direction) {
+            case "previous-day":
+                setCurrentDate(new Date(new Date(currentDate).setDate(new Date(currentDate).getDate() - 1)))
+                break
+            case "next-day":
+                setCurrentDate(new Date(new Date(currentDate).setDate(new Date(currentDate).getDate() + 1)))
+                break
+        }
+    }
+
+    const todayIndicator = () => {
+        return todaysDate === new Date(currentDate).toDateString()? "rgb(0, 230, 0)" : "red"
+    }
 
     return (
         <div className="homepage">
             <div className="homepage-header"> 
                 <div className="homepage-header-row1">
-                    <Header/>
+                    <Header setCurrentDate={setCurrentDate}/>
                 </div>
 
                 <div className="homepage-header-row2">
-                    <button type="button" className="yesterday-btn" > <i className="fa fa-arrow-left" aria-hidden="true"></i> </button>
-                    <strong> <span className="homepage-day">{currentDay}'s</span> Tasks </strong>
-                    <button type="button" className="tomorrow-btn" > <i className="fa fa-arrow-right" aria-hidden="true"> </i> </button>
+                    <button type="button" className="yesterday-btn" onClick={() => handleDayNavigation("previous-day")}> <i className="fa fa-arrow-left" aria-hidden="true"></i> </button>
+                    <strong> <span className="homepage-day" style={{"color":todayIndicator()}}>{currentDay}'s</span> Tasks </strong>
+                    <button type="button" className="tomorrow-btn" onClick={() => handleDayNavigation("next-day")} > <i className="fa fa-arrow-right" aria-hidden="true"> </i> </button>
                 </div>
 
                 <div className="homepage-header-row3">
@@ -62,7 +79,7 @@ function HomePage ({isLoggedIn}) {
             <div className="homepage-body"> 
                 <div className="timeslots"> </div>
                 <ol id="task-list" className="task-list">
-                    {tasks.length==0? null:tasks.map((task)=> 
+                    {homePageTasks.length==0? null:homePageTasks.map((task)=> 
                         <li align="left" key={task.id}>
                             <TaskCard task={task} handleDeleteEntity={handleDeleteEntity}/>
                         </li>
