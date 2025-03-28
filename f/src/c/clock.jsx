199cad5a6b5f4503885d7bf89b9e594df23cd4dc
Favour Umeh <favourstpa@gmail.com>
2025-03-28 11:41:54 +0000

    Updated f/src/c/clock.jsx: Refactored Clock component to include sitePage context for current date management; updated date indicator logic and ensured current date changes to today's date when not on the homepage.

--- f/src/c/clock.jsx
+++ f/src/c/clock.jsx
@@ -2,31 +2,35 @@ import { useState, useEffect, useContext } from "react"
 import globalContext from "../context"
 
 const Clock = () => {
-  const [todayDateTime, setTodayDateTime] = useState(new Date())
-  const {currentDate} = useContext(globalContext)
-  // Update the time every second
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTodayDateTime(new Date())
-    }, 1000)
+    const [todayDateTime, setTodayDateTime] = useState(new Date())
+    const {currentDate, setCurrentDate, sitePage} = useContext(globalContext)
+    // Update the time every second
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setTodayDateTime(new Date())
+      }, 1000)
 
-    // Cleanup the interval on component unmount
-    return () => clearInterval(intervalId)
-  }, [])
+      // Cleanup the interval on component unmount
+      return () => clearInterval(intervalId)
+    }, [])
 
-  // Format the time as a string (e.g., "12:34:56 PM")
-  const formatTime = (date) => {
-    return date.toLocaleTimeString()
-  }
+    // Format the time as a string (e.g., "12:34:56 PM")
+    const formatTime = (date) => {
+      return date.toLocaleTimeString()
+    }
 
-  const fromatDate = (date) => {
-    return date.toJSON().slice(0, 10)
-  }
-  const todayIndicator = () => {
-    return todayDateTime.toDateString() === new Date(currentDate).toDateString()? "white" : "red"
-}
+    const fromatDate = (date) => {
+      return date.toJSON().slice(0, 10)
+    }
+    const todayIndicator = () => {
+      return todayDateTime.toDateString() === new Date(currentDate).toDateString()? "white" : "red"
+    } 
 
-
+    useEffect(() => {
+        if (sitePage !== "view-homepage") {
+            setCurrentDate(new Date())
+        }
+    }, [sitePage])
   return (
       <div className="clock"> <span style={{color:todayIndicator()}}>{fromatDate(new Date(currentDate))} </span>| {formatTime(todayDateTime)}</div>
   )
