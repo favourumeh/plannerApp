db95eef1290d2b013cfe0bef833795c77bc8579b
Favour Umeh <favourstpa@gmail.com>
2025-04-04 15:32:36 +0100

    Updated b/plannerPackage/project_config.py:  access and refresh token durations to 1day and 7days respectively

--- b/plannerPackage/project_config.py
+++ b/plannerPackage/project_config.py
@@ -3,8 +3,8 @@ allowed_origins= [
     "http://localhost:5173"
     ]
 
-access_token_dur = 30 #(~10mins)
-refresh_token_dur =  3600/86400 #(~1hr)
+access_token_dur =  24*60 #(1day) #(~10mins)
+refresh_token_dur =  7 #(7days) #3600/86400 #(~1hr)
 
 #generate a new secret key 
 #from uuid import uuid4
