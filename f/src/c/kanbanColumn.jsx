b48f8ac32d7fe63d37d5037038b05f09455ae3cc
Favour Umeh <favourstpa@gmail.com>
2025-04-16 17:58:36 +0100

    Updated f/src/c/kanbanColumn.jsx: integrated hover text functionality for Kanban Cards.

--- f/src/c/kanbanColumn.jsx
+++ f/src/c/kanbanColumn.jsx
@@ -2,10 +2,12 @@ import "./kanbanColumn.css"
 import { useState, useEffect, useContext } from "react"
 import KanbanCard from "./kanbanCard"
 import { useDroppable } from "@dnd-kit/core"
+import globalContext from "../context"
 
 export default function KanbanColumn ({columnId, columnTitle, entityArr, entityName}) {
-    if (entityName !="task" && columnId ==  "Paused") return
+    if (entityName !=="task" && columnId ===  "Paused") return
     const [columnEntityArr, setColumnEntityArr] = useState([])
+    const {onShowHoverText, onHideHoverText} = useContext(globalContext)
 
     useEffect ( () => {
         setColumnEntityArr(entityArr.filter((entity) => entity.status==columnId))
@@ -13,13 +15,22 @@ export default function KanbanColumn ({columnId, columnTitle, entityArr, entityN
 
     const {setNodeRef} = useDroppable({ id: columnId }) //dnd
 
-
+    const getHoverText = (entity) => {
+        if (entityName !=="task") {
+            return "Title: " + entity.title + "\nDesc: " + entity.description
+        } 
+        return "Desc: " + entity.description
+    }
     return (
         <>
             <div ref = {setNodeRef} id={columnId} className="kanban-column"> {`${columnTitle} [${columnEntityArr.length}]`} 
                 {columnEntityArr.map( 
                     (columnEntity) => 
-                        <div className="kanban-item-overlay">
+                        <div 
+                            className="kanban-item-overlay"
+                            onMouseEnter={() => onShowHoverText(getHoverText(columnEntity))}
+                            onMouseLeave={onHideHoverText}
+                        >
                             <KanbanCard entity={columnEntity} entityName={entityName}/> 
                         </div>
                     )}
