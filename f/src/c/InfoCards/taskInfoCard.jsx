ba4d0ba40ba9ed2aced00192b1b483d99f506c7b
Favour Umeh <favourstpa@gmail.com>
2025-04-21 20:14:21 +0100

    Bug fix f/src/c/InfoCards/taskInfoCard.jsx: Refactored getTaskFinishDate function in TaskInfoCard component to ensure task.start is checked before processing finish date calculation.

--- f/src/c/InfoCards/taskInfoCard.jsx
+++ f/src/c/InfoCards/taskInfoCard.jsx
@@ -11,10 +11,13 @@ const TaskInfoCard = ({task, translate, taskObjective, taskProject}) => {
         if (!!task.finish){
             return task.finish
         }
-        const start = new Date(task.start)
-        const tzOffsetMS = start.getTimezoneOffset()*60*1000
-        const finish = new Date( start.getTime() - tzOffsetMS + task.duration*60*1000) // (currentTz +/- offset ) added timezone offset to put finish a hour ahead actual finish time in the current tz
-        return finish.toISOString().replace(/:\d{2}\.\d{3}Z$/, '') // (currentTz +/- offset -/+offset )  toISOString converts the date to UTC (so for BTS: minus 1hr which is in BTS))
+        if (!!task.start){
+            const start = new Date(task.start)
+            const tzOffsetMS = start.getTimezoneOffset()*60*1000
+            const finish = new Date( start.getTime() - tzOffsetMS + task.duration*60*1000) // (currentTz +/- offset ) added timezone offset to put finish a hour ahead actual finish time in the current tz
+            return finish.toISOString().replace(/:\d{2}\.\d{3}Z$/, '') // (currentTz +/- offset -/+offset )  toISOString converts the date to UTC (so for BTS: minus 1hr which is in BTS))
+        }
+
     }
 
     return (
