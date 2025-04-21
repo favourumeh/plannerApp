bd4e89332295809391acc7d9a7c0767b63da0f08
Favour Umeh <favourstpa@gmail.com>
2025-04-21 12:41:39 +0100

    Created staticVariables.js: Added static variables for default entities and color dictionary

---
+++ f/src/staticVariables.js
@@ -0,0 +1,40 @@
+
+//default entities
+const defaultTask = {
+    isRecurring:false, 
+    priorityScore:1, 
+    status:"To-Do",
+    description:"", 
+    duration:10, 
+    scheduledStart: "", 
+    start:"", 
+    finish:"",
+    tag:""
+}
+
+const defaultObjective = {
+    status:"To-Do",
+    description:"", 
+    title:"",
+    deadline:"",
+    tag:""
+}
+
+const defaultProject = {
+    status:"To-Do",
+    description:"", 
+    title:"",
+    deadline:"",
+    tag:""
+}
+
+// colour dict for the progress page
+const colourDict = {
+    "red":"rgba(255, 59, 48, 0.8)",
+    "orange": "rgba(255, 149, 0, 0.8)",
+    "yellow":"rgba(255, 204, 0, 0.8)",
+    "light-green": "rgba(52, 199, 89, 0.6)",
+    "green": "rgba(48, 209, 88, 0.8)"
+}
+
+export {defaultTask, defaultObjective, defaultProject, colourDict}
