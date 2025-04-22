f533d6761cfa69f351918a89a592d226c1244c90
Favour Umeh <favourstpa@gmail.com>
2025-04-22 14:28:22 +0100

    Updated f/src/c/InfoCards/projectInfoCard.jsx: Corrected labels for project title and deadline for clarity.

--- f/src/c/InfoCards/projectInfoCard.jsx
+++ f/src/c/InfoCards/projectInfoCard.jsx
@@ -8,9 +8,9 @@ const ProjectInfoCard = ({project, translate}) => {
     project = formatDateFields(project)
     return (
             <InfoCard translate={translate}>
-                <div className='info-card-item info-card-project-title'><span>Desc:</span> {project.title}</div>
+                <div className='info-card-item info-card-project-title'><span>Title:</span> {project.title}</div>
                 <div className='info-card-item info-card-project-description'><span>Desc:</span> {project.description}</div>
-                <div className='info-card-item info-card-project-dealine'><span>Finish:</span> {project.deadline}</div>
+                <div className='info-card-item info-card-project-deadline'><span>Deadline:</span> {project.deadline}</div>
             </InfoCard>
     )
 }
