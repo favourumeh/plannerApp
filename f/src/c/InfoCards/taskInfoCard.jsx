ca13c547a16fa572363d764ef849d1949346ef9b
Favour Umeh <favourstpa@gmail.com>
2025-04-22 18:59:25 +0100

    Updated  f/src/c/taskInfoCard.jsx: Improved date formatting for task start and finish fields

--- f/src/c/InfoCards/taskInfoCard.jsx
+++ f/src/c/InfoCards/taskInfoCard.jsx
@@ -7,16 +7,25 @@ const TaskInfoCard = ({task, translate, taskObjective, taskProject}) => {
     const {formatDateFields} = useContext(globalContext)
 
     task = formatDateFields(task)
-    const getTaskFinishDate = () => {
-        if (!!task.finish){
-            return task.finish
-        }
-        if (!!task.start){
+
+    const formatTaskStart = () => {
+        if (!task.start) return task.start
+        return `${task.start.split("T")[1]} (${task.start.split("T")[0]})`
+    }
+    const formatTaskFinish = () => {
+        if (!task.finish && !task.start) return task.finish
+        
+        if (!!task.start){ //calculate estimated finish
             const start = new Date(task.start)
             const tzOffsetMS = start.getTimezoneOffset()*60*1000
             const finish = new Date( start.getTime() - tzOffsetMS + task.durationEst*60*1000) // (currentTz +/- offset ) added timezone offset to put finish a hour ahead actual finish time in the current tz
-            return finish?.toISOString().replace(/:\d{2}\.\d{3}Z$/, '') // (currentTz +/- offset -/+offset )  toISOString converts the date to UTC (so for BTS: minus 1hr which is in BTS))
+            const formatted_finish = finish?.toISOString().replace(/:\d{2}\.\d{3}Z$/, '') // (currentTz +/- offset -/+offset )  toISOString converts the date to UTC (so for BTS: minus 1hr which is in BTS))
+            return `${formatted_finish.split("T")[1]} (${formatted_finish.split("T")[0]})`
+
         }
+
+        return `${task.finish.split("T")[1]} (${task.finish.split("T")[0]})`
+
 
     }
 
@@ -24,11 +33,13 @@ const TaskInfoCard = ({task, translate, taskObjective, taskProject}) => {
             <InfoCard translate={translate}>
                 <div className='info-card-item info-card-task-project-title'><span>Project Title:</span> {taskProject.title}</div>
                 <div className='info-card-item info-card-task-objective-title'><span>Objective Title:</span> {taskObjective.title}</div>
+                <div className='info-card-item info-card-task-status'><span>Status:</span> {task.status}</div>
                 <div className='info-card-item info-card-task-description'><span>Desc:</span> {task.description}</div>
                 <div className='info-card-item info-card-task-scheduled-date'><span>Scheduled Start:</span> {task.scheduledStart}</div>
-                <div className='info-card-item info-card-task-duration-est'><span>Duration:</span> {task.durationEst}mins</div>
-                <div className='info-card-item info-card-task-start'><span> Start:</span> {task.start}</div>
-                <div className='info-card-item info-card-task-finish'><span>Finish:</span> {getTaskFinishDate()}</div>
+                <div className='info-card-item info-card-task-duration-est'><span>Duration (est):</span> {task.durationEst}mins</div>
+                <div className='info-card-item info-card-task-duration'><span>Duration:</span> {task.duration}mins</div>
+                <div className='info-card-item info-card-task-start'><span> Start:</span> {formatTaskStart()}</div>
+                <div className='info-card-item info-card-task-finish'><span>Finish:</span> {formatTaskFinish()}</div>
             </InfoCard>
     )
 }
