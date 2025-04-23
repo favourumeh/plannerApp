f614e18e77e2cf96af272d30d1089949d04504a3
Favour Umeh <favourstpa@gmail.com>
2025-04-23 13:00:56 +0100

    Created f/src/c/timerLine.jsx/css: Created and style a component that conveys the passage of time in the homepage.

---
+++ f/src/c/timerLine.jsx
@@ -0,0 +1,42 @@
+import "./timerLine.css"
+import {useState, useEffect, useContext} from 'react'
+import globalContext from "../context"
+
+const TimerLine = ({bodyTop}) => {
+    const {userSettings} = useContext(globalContext)
+    const currentDateStr = new Date().toDateString()
+    const startOfDay =  new Date(currentDateStr + " " + userSettings.dayStartTime).getTime()
+    const endOfDay =  new Date(currentDateStr + " " + userSettings.dayEndTime).getTime()
+    
+    if (new Date().getTime() < startOfDay || new Date().getTime() > endOfDay) return 
+
+    const [currentDateTime, setCurrentDateTime] = useState(new Date())
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentDateTime(new Date())
+        }, 1000)
+        return () => clearInterval(intervalId)
+    })
+
+    const getTimerLinePosition = () => {
+        const elapsedTimeMS = currentDateTime.getTime() - startOfDay
+        // const elapsedTimeMS = new Date("2025-04-23 08:00").getTime() - startOfDay
+        const elapsedTimeMins = elapsedTimeMS / (60*1000)
+        const pixelsPerMinute = 15/10 // (15px for 10 mins)
+        const halfTimerLineContainerHeight = 12
+        const postion = bodyTop + elapsedTimeMins*pixelsPerMinute - halfTimerLineContainerHeight
+        return String(postion)
+    }
+
+    const timerContinerStyle = {"top": getTimerLinePosition() + "px"}
+
+    return (
+        <div style={timerContinerStyle} className='timer-line-container'>
+            <div className="timer-line-caret"><i class="fa fa-caret-right" aria-hidden="true"></i></div>
+            <div className="timer-line"></div>
+            <div className="timer-line-caret"><i class="fa fa-caret-left" aria-hidden="true"></i></div>
+        </div>
+    );
+};
+
+export default TimerLine;
