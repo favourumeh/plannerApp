851fb1ea2cb200891e6730a018fc0c4c36f7857d
Favour Umeh <favourstpa@gmail.com>
2025-04-27 13:17:45 +0100

    Updated f/src/project_config.js: Renamed backend_env to app_env for simplicity. Also added "prod-local" env option to conditional logic.

--- f/src/project_config.js
+++ f/src/project_config.js
@@ -1,15 +1,15 @@
 console.log(import.meta.env) //env variables defined in f/.env #
-const backend_env = import.meta.env.VITE_BACKEND_ENV
+const app_env = import.meta.env.VITE_APP_ENV
 var backendBaseUrl //build fails if this is not decalared
-if (backend_env == "dev") {
+if (app_env === "dev" || app_env === "prod-local") {
     backendBaseUrl = import.meta.env.VITE_DEV_BACKEND_API_URL
 }
-else if (backend_env == "prod") {
+else if (app_env === "prod") {
     backendBaseUrl = import.meta.env.VITE_PROD_BACKEND_API_URL
 }
 else {
-    console.log("Error backendBaseURL is not specified. Check frontend/.env and frontend/src/project_config.js")
+    console.log("Error backendBaseURL is not specified. Check f/.env and f/src/project_config.js")
 }
-console.log("backend_env:", backend_env)
+console.log("app_env:", app_env)
 console.log("backendbaseurl: ", backendBaseUrl )
 export {backendBaseUrl}
