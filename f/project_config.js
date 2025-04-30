45885155f7e44bfc8936856c069202aea31574f5
Favour Umeh <favourstpa@gmail.com>
2025-04-30 13:27:19 +0100

    Updated f/project_config.js and .gitigore: simplified config file's backend url handling by leveraging VITE's ability to automatically distinguish .env.development and .env.production files.

--- f/project_config.js
+++ f/project_config.js
@@ -1,15 +1,4 @@
 console.log(import.meta.env) //env variables defined in f/.env #
-const app_env = import.meta.env.VITE_APP_ENV
-var backendBaseUrl //build fails if this is not decalared
-if (app_env === "dev" || app_env === "prod-local") {
-    backendBaseUrl = import.meta.env.VITE_DEV_BACKEND_API_URL
-}
-else if (app_env === "prod") {
-    backendBaseUrl = import.meta.env.VITE_PROD_BACKEND_API_URL
-}
-else {
-    console.log("Error backendBaseURL is not specified. Check f/.env and f/src/project_config.js")
-}
-console.log("app_env:", app_env)
+const backendBaseUrl = import.meta.env.VITE_BACKEND_API_URL
 console.log("backendbaseurl: ", backendBaseUrl )
 export {backendBaseUrl}
