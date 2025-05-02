1f256fdefe2384d4506e8e8dfda47531515efa56
Favour Umeh <favourstpa@gmail.com>
2025-05-02 13:30:24 +0100

    Updated f/adjustTitle.js: Added prod-local-docker if logic

--- f/adjustTitle.js
+++ f/adjustTitle.js
@@ -1,10 +1,11 @@
 
 const app_env = import.meta.env.VITE_APP_ENV
+console.log("app_env", app_env)
 
 let siteTitleTag = document.getElementById("site-title")
 if (app_env === "dev") {
-    siteTitleTag.innerText = "dev - PlannerApp "
-} else if (app_env ==="prod-local") {
+    siteTitleTag.innerText = "dev - PlannerApp"
+} else if (["prod-local-docker", "prod-local"].includes(app_env)) {
     siteTitleTag.innerText = "prod-local - PlannerApp"
 } else {
     siteTitleTag.innerText = "PlannerApp"
