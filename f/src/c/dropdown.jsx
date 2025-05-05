7de6c2e1f67cf2e9743526725fe316b9deb2cfc9
Favour Umeh <favourstpa@gmail.com>
2025-05-05 15:46:20 +0100

    Updated f/src/c/dropdown.jsx: Added buttonClassName prop to Dropdown component for customizable button styling

--- f/src/c/dropdown.jsx
+++ f/src/c/dropdown.jsx
@@ -1,9 +1,9 @@
 import "./dropdown.css"
 
-const Dropdown = ({buttonContent, translate, children}) => {
+const Dropdown = ({buttonContent, buttonClassName, translate, children}) => {
     return (
         <div className="dropdown">
-            <button className="dropbtn">{buttonContent}</button>
+            <button className={`dropbtn ${buttonClassName}`}>{buttonContent}</button>
             <div style={{"translate": translate}} className="dropbtn-content">
                 {children}
             </div>
