e2d4b95e21caeab929302e68e7f68f76919a8f03
Favour Umeh <favourstpa@gmail.com>
2025-05-13 14:52:51 +0100

    Updated readTasksObjectiveAndProjectQueryOption.js: Added handleLogout parameter to the query function and edited query key

--- f/src/queryOptions/readTasksObjectiveAndProjectQueryOption.js
+++ f/src/queryOptions/readTasksObjectiveAndProjectQueryOption.js
@@ -1,10 +1,10 @@
 import { queryOptions, keepPreviousData } from "@tanstack/react-query"
 import { fetchTasksObjectiveAndProject } from "../fetch_entities"
 
-export default function readTasksObjectiveAndProjectQueryOption(taskId, handleNotification) {
+export default function readTasksObjectiveAndProjectQueryOption(taskId, handleNotification, handleLogout) {
     return queryOptions({
-        queryKey: [`projAndObjByTaskId`, taskId],
-        queryFn: () =>  fetchTasksObjectiveAndProject(taskId, handleNotification),
+        queryKey: [`proj-and-obj-from-task-id-${taskId}`, taskId],
+        queryFn: () =>  fetchTasksObjectiveAndProject(taskId, handleNotification, handleLogout),
         placeholderData: keepPreviousData, 
         retry: 3,
     })
