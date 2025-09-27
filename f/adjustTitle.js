aad0a5efb6459be680bda73288baa97681fea1e0
Favour Umeh <favourstpa@gmail.com>
2025-09-27 20:17:28 +0100

    Testing github webhook deployment: Fixed unchanged title of the site running on raspberry pi to 'rasp-pi - plannerApp'

--- f/adjustTitle.js
+++ f/adjustTitle.js
@@ -6,7 +6,7 @@ let siteTitleTag = document.getElementById("site-title")
 if (app_env === "dev") {
     siteTitleTag.innerText = "dev - PlannerApp"
 } else if (["prod-local"].includes(app_env)) {
-    siteTitleTag.innerText = "prod-local - PlannerApp"
+    siteTitleTag.innerText = "rasp-pi - PlannerApp"
 }else if (app_env == "prod-local-docker") {
     siteTitleTag.innerText = "rasp-pi - plannerApp"
 }else {
